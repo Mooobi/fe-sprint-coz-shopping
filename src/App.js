96f68c892b44679875c06785273e30ddaed30d5f
Mooobi <qkrantod@gmail.com>
2023-05-13 15:46:41 +0900

    Implement productComponent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Dummy from './Dummy';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Dummy />
+    </>
+  );
 }
 
 export default App;
