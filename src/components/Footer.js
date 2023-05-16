7d56c5f4507a5173a44306af4a1157591d2d1677
Mooobi <qkrantod@gmail.com>
2023-05-16 12:29:36 +0900

    Fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,10 @@ import classes from './Footer.module.css';
 const Footer = () => {
   return (
     <>
-      <div className={classes.wrapper}></div>
-      <section className={classes.footer}>
+      <footer className={classes.footer}>
         <p>개인정보 처리방침 | 이용 약관</p>
         <p>All rights reserved @ Codestates</p>
-      </section>
+      </footer>
     </>
   );
 };
